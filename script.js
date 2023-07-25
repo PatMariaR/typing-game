07b196eab1e4fa42d9057622cf199d8aa96c6fe3
PatMariaR <patriciarocha.projects@gmail.com>
2023-07-25 15:53:16 +0100

    add Timer is JS

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const quote = document.getElementById('quote');
 const input = document.getElementById('typed-value');
 const start = document.getElementById('start'); //<button id="start"/>
+const message = document.getElementById('message');
+
 
 let quoteText;
 let wordQueue; //new variable assignment. we want a several words to type correctly
 let highlightPosition;
+let startTime;
 
 function startGame() {
     //quote.innerHTML = `<span>${targetWord}</span>`;
@@ -18,12 +21,14 @@ function startGame() {
     //quote.childNodes = ['<span>type</span>', '<span>me</span>']
     quote.childNodes[highlightPosition].className = 'highlight';
 
+    startTime = new Date().getTime();
 }
 
 function checkInput(){     // we are reading/manipulating the value;
     const currentWord = wordQueue[0]; //first word in the wordqueue array;
     const typedValue = input.value.trim(); //JS string method -> controla os espaços a +. 
      
+    //validate user's typing for errors
     if (currentWord !== typedValue) { //false when the word typed is correct;
         input.className = currentWord.startsWith(typedValue) ? "" : "error";
         
@@ -36,9 +41,29 @@ function checkInput(){     // we are reading/manipulating the value;
         
         return;
     }
-    wordQueue.shift(); //shift removes first item(0th element)
+    
+    //this happens when there is no error...
+    wordQueue.shift(); //shift removes first item(0th element)/removing each one as we go. 
     input.value = ''; //empty textbox. we are assigning the value
 
+    quote.childNodes[highlightPosition].className = ""; // unhighlight word;
+
+    if (wordQueue.length === 0) { //if we have riun out of words then game over.
+        gameOver();
+        return;
+    }
+
+
+    highlightPosition++;
+    quote.childNodes[highlightPosition].className = 'highlight';
+
 }
+
+function gameOver() {
+    const elapsedTime = new Date().getTime() - startTime;
+    message.innerHTML = `<span class="congrats">Congratulations!</span><br>You finished in ${elapsedTime / 1000} seconds. `;
+
+};
+
 start.addEventListener('click', startGame);
 input.addEventListener('input', checkInput);
