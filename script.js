2e64154e225aaef5326d719fe0c442ee2e5b8e26
PatMariaR <patriciarocha.projects@gmail.com>
2023-07-26 00:05:16 +0100

    update JS

--- script.js
+++ script.js
@@ -3,15 +3,28 @@ const input = document.getElementById('typed-value');
 const start = document.getElementById('start'); //<button id="start"/>
 const message = document.getElementById('message');
 
+const quotes = [
+    'Things are only impossible until they are not',
+    'It is possible to commit no errors and still lose. That is not a weakness. That is life', 
+    'There is a way out of every box, a solution to every puzzle; it is just matter of finding it.',
+    'Without freedom of choice there is no creativity',
+    'Logic is the beginning of wisdom, not the end',
+    'Improve a mechanical device and you may double productivity. But improve yourself, you gain a thousandfold',
+    'Compassion: that is the one thing no machine ever had. Maybe it is the one thing that keeps us ahead of them.',
+]
 
-let quoteText;
+
+//let quoteText;
 let wordQueue; //new variable assignment. we want a several words to type correctly
 let highlightPosition;
 let startTime;
 
 function startGame() {
+
+    const quoteIndex = Math.floor(Math.random()*quotes.length);
+    const quoteText = quotes[quoteIndex];
     //quote.innerHTML = `<span>${targetWord}</span>`;
-    quoteText = 'type me';
+   // quoteText = 'type me';
     wordQueue = quoteText.split(' '); // separa as palavras; array of words ['type', 'me'];
     quote.innerHTML = wordQueue.map(word => (`<span>${word}</span>`)).join(''); //<span>type</span><span>me</span>
     //.map -> array method
@@ -25,7 +38,8 @@ function startGame() {
 }
 
 function checkInput(){     // we are reading/manipulating the value;
-    const currentWord = wordQueue[0]; //first word in the wordqueue array;
+    const currentWord = wordQueue[0].replaceAll(".", "").replaceAll(",", ""); //first word in the wordqueue array;
+    //relaceAll -> clear the ponctuation;
     const typedValue = input.value.trim(); //JS string method -> controla os espaços a +. 
      
     //validate user's typing for errors
@@ -41,19 +55,16 @@ function checkInput(){     // we are reading/manipulating the value;
         
         return;
     }
-    
+   
     //this happens when there is no error...
     wordQueue.shift(); //shift removes first item(0th element)/removing each one as we go. 
     input.value = ''; //empty textbox. we are assigning the value
-
-    quote.childNodes[highlightPosition].className = ""; // unhighlight word;
 
     if (wordQueue.length === 0) { //if we have riun out of words then game over.
         gameOver();
         return;
     }
-
-
+    quote.childNodes[highlightPosition].className = ""; // unhighlight word;
     highlightPosition++;
     quote.childNodes[highlightPosition].className = 'highlight';
 
