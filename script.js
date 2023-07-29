56eac6be2c0a6b79aede3a0d9c2db9a1e06d342a
PatMariaR <patriciarocha.projects@gmail.com>
2023-07-30 00:50:23 +0100

    add CSS

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const quote = document.getElementById('quote');
-const input = document.getElementById('typed-value');
+const input = document.getElementById('typed-value'); //<input id= typed-value"
 const start = document.getElementById('start'); //<button id="start"/>
 const message = document.getElementById('message');
 
 const quotes = [
-    'Things are only impossible until they are not',
-    'It is possible to commit no errors and still lose. That is not a weakness. That is life', 
-    'There is a way out of every box, a solution to every puzzle; it is just matter of finding it.',
-    'Without freedom of choice there is no creativity',
-    'Logic is the beginning of wisdom, not the end',
-    'Improve a mechanical device and you may double productivity. But improve yourself, you gain a thousandfold',
-    'Compassion: that is the one thing no machine ever had. Maybe it is the one thing that keeps us ahead of them.',
+    'Type me',
+//    'Things are only impossible until they are not',
+//     'It is possible to commit no errors and still lose. That is not a weakness. That is life', 
+//    'There is a way out of every box, a solution to every puzzle; it is just matter of finding it.',
+//     'Without freedom of choice there is no creativity',
+//     'Logic is the beginning of wisdom, not the end',
+//     'Improve a mechanical device and you may double productivity. But improve yourself, you gain a thousandfold',
+//     'Compassion: that is the one thing no machine ever had. Maybe it is the one thing that keeps us ahead of them.',
 ]
-
 
 //let quoteText;
 let wordQueue; //new variable assignment. we want a several words to type correctly
@@ -22,10 +22,12 @@ let startTime;
 function startGame() {
 
     const quoteIndex = Math.floor(Math.random()*quotes.length);
+   
     const quoteText = quotes[quoteIndex];
     //quote.innerHTML = `<span>${targetWord}</span>`;
    // quoteText = 'type me';
     wordQueue = quoteText.split(' '); // separa as palavras; array of words ['type', 'me'];
+    
     quote.innerHTML = wordQueue.map(word => (`<span>${word}</span>`)).join(''); //<span>type</span><span>me</span>
     //.map -> array method
     //make a queue of words
@@ -35,6 +37,11 @@ function startGame() {
     quote.childNodes[highlightPosition].className = 'highlight';
 
     startTime = new Date().getTime();
+
+    document.body.className = "";
+   
+    start.className = "started";
+    input.focus();
 }
 
 function checkInput(){     // we are reading/manipulating the value;
@@ -72,6 +79,9 @@ function checkInput(){     // we are reading/manipulating the value;
 
 function gameOver() {
     const elapsedTime = new Date().getTime() - startTime;
+    
+    document.body.className = "winner";
+
     message.innerHTML = `<span class="congrats">Congratulations!</span><br>You finished in ${elapsedTime / 1000} seconds. `;
 
 };
